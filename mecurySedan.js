0d64be545fb2028f4522f76ebd4cb49a6034a125
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-05 17:15:17 -0400

    scheduleService function works, tested out by changing mileages and it's working!

--- mecurySedan.js
+++ mecurySedan.js
@@ -7,34 +7,40 @@ import Vehicle from "./vehicleBaseClass.js";
 class Car extends Vehicle {
     constructor(make, model, year, color, mileage) {
         super(make, model, year, color, mileage)
-        this.maximumPassengers = 6
+        this.maximumPassengers = 6 //6 ppl? thats illegal xD
         this.passenger = 0
         this.numberOfWheels = 4
         this.maximumSpeed = 200
         this.fuel = 100
-        this.scheduleService = false
+        this.scheduleServices = false
+        // this.mileage = 50000
     }
 
     loadPassenger(num) {
 
     }
 
-    start() {
-        if (this.fuel > 0) {
-            console.log("engine started...!!!");
-        } else {
-            console.log("engine cannot start...");
-        }
-    }
+    // start() {
+    //     if (this.fuel > 0) {
+    //         console.log("engine started...!!!");
+    //     } else {
+    //         console.log("engine cannot start...");
+    //     }
+    // }
 
     scheduleService(mileage) {
-
+        if(this.mileage > 30000) {
+            console.log("time to get some service")
+        } else {
+            console.log("drive safe!")
+        }
     }
 
 }
 
-let v = new Car("Mecury", "Sedan", "1965", "color", "mileage");
+let v = new Car("Mecury", "Sedan", "1965", "color", 500000);
 console.log(v.make)
 console.log(v.model)
 v.accelerate()
 v.start()
+v.scheduleService()
